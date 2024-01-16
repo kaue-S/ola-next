7dd54c3d724758e7c4a82bbb3a95f983bf126ac8
Kauê Evangelista da Silva Aguiar <kauesilva38@hotmail.com>
2024-01-16 11:48:35 -0300

    comit

---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import Menu from "@/components/menu";
+export default function Home() {
+  return (
+    <>
+      <h1>Olá next.js!</h1>
+      <Menu />
+      <main>
+        <h2>Bem-Vindo ao Next.js</h2>
+        <h3>Página Inicial</h3>
+      </main>
+    </>
+  );
+}
