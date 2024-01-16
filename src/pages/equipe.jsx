800726ed92cc5f548c8ea44cb8ada83b72dda4e0
Kauê Evangelista da Silva Aguiar <kauesilva38@hotmail.com>
2024-01-16 12:02:27 -0300

    comit

---
+++ src/pages/equipe.jsx
@@ -0,0 +1,18 @@
+import Menu from "@/components/menu";
+
+export default function Equipe() {
+  return (
+    <>
+      <h1>Olá next.js!</h1>
+      <Menu />
+      <main>
+        <h2>Equipe</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit itaque
+          accusamus doloribus corporis delectus deserunt ducimus ipsam, porro
+          nam molestiae accusantium dolor sapiente qui nulla!
+        </p>
+      </main>
+    </>
+  );
+}
---
+++ src/pages/servicos.jsx
@@ -0,0 +1,18 @@
+import Menu from "@/components/menu";
+
+export default function Servicos() {
+  return (
+    <>
+      <h1>Olá next.js!</h1>
+      <Menu />
+      <main>
+        <h2>Serviços</h2>
+        <ol>
+          <li>Bla Bla Bla</li>
+          <li>Bla Bla Bla</li>
+          <li>Bla Bla Bla</li>
+        </ol>
+      </main>
+    </>
+  );
+}
